72f1ebafa91406f94cdf3b7c0118338a060a7880
Christian Aucane <christianaucane1@gmail.com>
2023-12-19 12:11:03 +0100

    Syncronize countdown and play round

--- managers/game.py
+++ managers/game.py
@@ -14,17 +14,71 @@ class Game:
     :param max_score(int): maximum score of the game (3 by default)
     """
 
+    classifier = ClassifierManager()
+
     def __init__(self, player1, player2, max_score=3):
+
         self._player1 = player1
         self._player2 = player2
-        self._score = {
-            "player1": 0,
-            "player2": 0
-        }
         self._max_score = max_score
+
+        self._players_data = {
+            player1: {
+                "clicked": False,
+                "image": None,
+                "score": 0,
+                "choice": None,
+                "event": asyncio.Event(),
+            },
+            player2: {
+                "clicked": False,
+                "image": None,
+                "score": 0,
+                "choice": None,
+                "event": asyncio.Event(),
+            }
+        }
+        self._round_data = {
+            "winner": None,
+
+            "update": False,
+            "update_lock": asyncio.Lock(),
+        }
 
     def __str__(self):
         return f"{self.player1} vs {self.player2}"
+
+    async def update(self):
+        async with self.get_round_data("update_lock"):
+            if self.get_round_data("update"):
+                for player in self._players_data:
+                    self.set_player_data(player, "clicked", False)
+                    self.set_player_data(player, "image", None)
+                    self.set_player_data(player, "event", asyncio.Event())
+                    self.set_player_data(player, "choice", None)
+
+                winner = self.get_round_data("winner")
+                self.set_round_data("winner", None)
+                self.add_point(winner)
+                self.set_round_data("update", False)
+            else:
+                self.set_round_data("update", True)
+
+    def set_player_data(self, player, key, value):
+        self._players_data[player][key] = value
+
+    def get_player_data(self, player, key):
+        return self._players_data[player].get(key)
+
+    def set_round_data(self, key, value):
+        self._round_data[key] = value
+
+    def get_round_data(self, key):
+        return self._round_data.get(key)
+
+    def add_point(self, player):
+        self.set_player_data(player, "score", self.get_player_data(player, "score") + 1)
+
 
     # ==== Attributes properties ====
 
@@ -37,25 +91,22 @@ class Game:
         return self._player2
 
     @property
-    def score(self):
-        return self._score
-
-    @property
     def max_score(self):
         return self._max_score
 
     # ==== Others properties ====
 
     @property
-    def winner(self):
+    def game_winner(self):
         """
         Check the winner of the game
         :return: name of the winner of the game or None if the game is not over
         """
-        if self.score["player1"] == self.max_score:
+        if self.get_player_data(self.player1, "score") == self.max_score:
             return self.player1
-        elif self.score["player2"] == self.max_score:
+        elif self.get_player_data(self.player2, "score") == self.max_score:
             return self.player2
+
 
     @property
     def status(self):
@@ -64,38 +115,73 @@ class Game:
         :return: True if the game is over, False otherwise
         """
         return {
-            "player1": self.player1,
-            "player2": self.player2,
-            "score": self.score,
+            "player1": {
+                "score": self.get_player_data(self.player1, "score"),
+                "name": self.player1
+            },
+            "player2": {
+                "score": self.get_player_data(self.player2, "score"),
+                "name": self.player2
+            },
             "max_score": self.max_score,
-            "winner": self.winner,
+            "winner": self.game_winner,
         }
-    # ==== Public methods ====
 
-    def play_round(self, player1, player2):
-        """
-        Play a round of the game
-        :param player1: Values can be 'rock', 'paper' or 'scissors'
-        :param player2: Values can be 'rock', 'paper' or 'scissors'
-        :return: name of the winner of the round
-        """
-        if player1 == "rock" and player2 == "scissors":
-            winner = "player1"
-        elif player1 == "scissors" and player2 == "rock":
-            winner = "player2"
-        elif player1 == "scissors" and player2 == "paper":
-            winner = "player1"
-        elif player1 == "paper" and player2 == "scissors":
-            winner = "player2"
-        elif player1 == "paper" and player2 == "rock":
-            winner = "player1"
-        elif player1 == "rock" and player2 == "paper":
-            winner = "player2"
+    def get_other_player(self, player):
+        if player == self.player1:
+            return self.player2
+        elif player == self.player2:
+            return self.player1
         else:
-            return "Equality"
+            raise ValueError("Player not found (get_other_player)")
 
-        self._score[winner] += 1
-        return getattr(self, winner)
+    async def play_round(self, player, image):
+        other_player = self.get_other_player(player)
+        prediction = self.classifier.predict(image)
+        if prediction == "neutral" or prediction is None:
+            return None, False
+        else:
+            self.set_player_data(player, "image", image)
+            self.set_player_data(player, "choice", prediction)
+            self.get_player_data(player, "event").set()
+            round_played = await self.__determine_round_winner()
+            if not round_played:
+                await self.get_player_data(other_player, "event").wait()
+
+            winner = self.get_round_data("winner")
+            await self.update()
+            return winner, True
+
+
+    async def __determine_round_winner(self):
+        """
+        Wait the 2 players to play and return the winner of the round
+        :return: the winner of the round
+        """
+        if self.get_round_data("winner") is not None:
+            return True
+        player_choices = (
+            self.get_player_data(self.player1, "choice"),
+            # self.get_player_data(self.player2, "choice")
+            "scissors"  # TODO : supprimer
+        )
+
+        rules = {
+            ("rock", "scissors"): self.player1,
+            ("scissors", "rock"): self.player2,
+            ("scissors", "paper"): self.player1,
+            ("paper", "scissors"): self.player2,
+            ("paper", "rock"): self.player1,
+            ("rock", "paper"): self.player2
+        }
+
+        winner = rules.get(player_choices)
+
+        if winner is not None:
+            self.set_round_data("winner", winner)
+            return True
+
+        return False
 
 
 class GameManager:
@@ -103,10 +189,9 @@ class GameManager:
     GameManager class
     """
 
-    DELAY_TO_CAPTURE_IMAGE_IN_SEC = 3
+    DELAY_TO_CAPTURE_IMAGE_IN_SEC = 2
 
     def __init__(self):
-        self._clf = ClassifierManager()
         self._waiting_players = []
         self._games = {}
         self._player_events = {}
@@ -126,7 +211,7 @@ class GameManager:
             ), None
         )
         if game_id is None:
-            raise ValueError("Player not found")
+            raise ValueError("Player not found (get_game_id_by_player)")
         return game_id
 
     def __get_game_status(self, game_id):
@@ -145,43 +230,37 @@ class GameManager:
         }
 
     async def __match_players(self):
-        """
-        Match the players in the queue and start new games if there are enough players
-        :return: None
-        """
-        while len(self._waiting_players) >= 2:
+        if len(self._waiting_players) >= 2:
             player1 = self._waiting_players.pop(0)
             player2 = self._waiting_players.pop(0)
-            game_id = self.start_game(player1, player2)
-            print(f"Game {game_id} started between {player1} and {player2}")
-
-            # Signale aux joueurs qu'ils ont rejoint une partie
+            await self.start_game(player1, player2)
             self._player_events[player1].set()
             self._player_events[player2].set()
+            return True
+        else:
+            return False
 
-    # ==== Public methods ====
+    def get_game(self, player):
+        game_id = self.__get_game_id_by_player(player)
+        return self._games[game_id]
+
+    def get_other_player(self, player):
+        game = self.get_game(player)
+        return game.get_other_player(player)
 
     async def add_player_to_queue(self, player):
-        """
-        Add a player to the queue and wait for him to join a game
-        :param player:
-        :return: game_id
-        """
         if player not in self._waiting_players:
             self._waiting_players.append(player)
             event = asyncio.Event()
             self._player_events[player] = event
+            game_created = await self.__match_players()
+            if not game_created:
+                await event.wait()
 
-            asyncio.ensure_future(self.__match_players())
-
-            # Attend que le joueur rejoigne une partie
-            await event.wait()
-
-            # Récupère le game_id de la partie du joueur
             game_id = self.__get_game_id_by_player(player)
-            return game_id
+            return self.__get_game_status(game_id)
 
-        raise ValueError("Player already in queue")
+        raise ValueError("Player already in queue or not enough players")
 
     async def start_game(self, player1, player2):
         """
@@ -191,30 +270,18 @@ class GameManager:
         :return: (str) unique game id
         """
         try:
-            if player1 in self._waiting_players and player2 in self._waiting_players:
-                game_id = str(uuid.uuid4())
-                game = Game(player1, player2)
-                self._games[game_id] = game
-                self._waiting_players.remove(player1)
-                self._waiting_players.remove(player2)
-                return game_id
+            game_id = str(uuid.uuid4())
+            game = Game(player1, player2)
+            self._games[game_id] = game
+            return game_id
         except KeyError:
-            raise ValueError("Player not found")
+            raise ValueError("Player not found (start_game)")
 
-    async def play_round(self, game_id, player1, player2):
-        """
-        Play a round of the game
-        :param game_id: unique game id
-        :param player1:
-        :param player2:
-        :return: (tuple) (winner, status) where winner is the player wins the round and status is the status of the game
-        """
-        try:
-            pred_player1 = self._clf.predict(player1)
-            pred_player2 = self._clf.predict(player2)
-            if pred_player1 == "neutral" or pred_player2 == "neutral":
-                return None, self.__get_game_status(game_id)
-            round_winner = self._games[game_id].play_round(pred_player1, pred_player2)
-            return round_winner, self.__get_game_status(game_id)
-        except KeyError:
-            raise ValueError("Game not found")
+    async def play_round(self, player, image):
+        print(f"GameManager.play_round(player={player})")
+        game_id = self.__get_game_id_by_player(player)
+        game = self._games[game_id]
+        round_winner, round_played = await game.play_round(player, image)
+        print(f"GameManager.play_round(): round_winner: {round_winner}", f"round_played: {round_played}")
+        game_status = self.__get_game_status(game_id)
+        return round_winner, round_played, game_status
--- managers/web_socket.py
+++ managers/web_socket.py
@@ -1,44 +1,151 @@
+import base64
 import json
 import uuid
 
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
 from fastapi import WebSocket
+
+from .time import TimeManager
+from .game import GameManager
 
 
 class WebSocketManager:
     def __init__(self):
-        self.active_connections = set()
+        self.active_connections = {}
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, websocket: WebSocket, player_id: str):
         await websocket.accept()
-        self.active_connections.add(websocket)
+        self.active_connections[player_id] = websocket
 
-    async def disconnect(self, websocket: WebSocket):
+    async def disconnect(self, websocket: WebSocket, player_id: str):
         await websocket.close()
-        self.active_connections.remove(websocket)
+        del self.active_connections[player_id]
 
-    async def send_message(self, message: str, websocket: WebSocket):
+    @staticmethod
+    async def send_message(message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
 
 class WebSocketHandler:
+    time_manager = TimeManager()
+    game_manager = GameManager()
+    manager = WebSocketManager()
+
     def __init__(self):
-        self.manager = WebSocketManager()
         self.player_id = None
 
     async def on_connect(self, websocket: WebSocket):
         self.player_id = str(uuid.uuid4())  # Créer un nouvel identifiant unique
-        await self.manager.connect(websocket)
+        await self.manager.connect(websocket, self.player_id)
         await self.manager.send_message(
             json.dumps({
-                "status": "connected",
+                "action": "connect",
                 "player_id": self.player_id
             }),
             websocket
         )
 
     async def on_disconnect(self, websocket: WebSocket):
-        await self.manager.disconnect(websocket)
+        # TODO : envoyer un message a l'autre joueur pour indiquer qu'il se déconnecte
+        await self.manager.disconnect(websocket, self.player_id)
 
-    async def on_receive(self, websocket: WebSocket, data: str):
-        if data == ...:
-            ...
+    async def on_receive(self, websocket: WebSocket, data: dict):
+        action = data.get("action")
+
+        if action == "start_game":
+            await self.handle_start_game(websocket)
+        elif action == "on_click_start_countdown":
+            await self.handle_start_countdown(websocket)
+        elif action == "capture_webcam":
+            image_data = data['image_data'].split(',')[1]
+            await self.handle_capture_webcam(websocket, image_data)
+        elif action == "get_actual_timestamp":
+            await self.handle_get_actual_timestamp(websocket)
+
+    async def handle_start_game(self, websocket: WebSocket):
+        game_data = await self.game_manager.add_player_to_queue(self.player_id)
+        await self.manager.send_message(
+            json.dumps({
+                "action": "start_game",
+                "player_id": self.player_id,
+                "game_data": game_data
+            }),
+            websocket
+        )
+
+    async def handle_start_countdown(self, websocket: WebSocket):
+        game = self.game_manager.get_game(self.player_id)
+        game.set_player_data(self.player_id, "clicked", True)
+
+        other_player = self.game_manager.get_other_player(self.player_id)
+        other_player_clicked = game.get_player_data(other_player, "clicked")
+        if other_player_clicked:
+            message = json.dumps({
+                "action": "start_countdown",
+                "start_timestamp": self.time_manager.now(),
+                "end_timestamp": self.time_manager.timestamp_after_delay(
+                    self.game_manager.DELAY_TO_CAPTURE_IMAGE_IN_SEC
+                )
+            })
+            await self.manager.send_message(message, websocket)
+            await self.manager.send_message(message, self.manager.active_connections[other_player])
+        else:
+            await self.manager.send_message(
+                json.dumps({
+                    "action": "wait_for_other_player"
+                }),
+                websocket
+            )
+            await self.manager.send_message(
+                json.dumps({
+                    "action": "wait_to_click",
+                }),
+                self.manager.active_connections[other_player]
+            )
+
+    async def handle_capture_webcam(self, websocket: WebSocket, image_data: str):
+
+        image = await self.convert_base64_to_image(image_data)
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        plt.show()
+
+        round_winner, round_played, game_status = await self.game_manager.play_round(self.player_id, image)
+        print("winner : ", round_winner)
+        if not round_played:
+            await self.manager.send_message(
+                json.dumps({
+                    "action": "invalid_image",
+                    "image_data": image_data
+                }),
+                websocket
+            )
+        else:
+
+            await self.manager.send_message(
+                json.dumps({
+                    "action": "capture_webcam",
+                    "image_data": image_data
+                }),
+                websocket
+            )
+
+    async def handle_get_actual_timestamp(self, websocket: WebSocket):
+        timestamp = self.time_manager.now()
+        await self.manager.send_message(
+            json.dumps({
+                "action": "get_actual_timestamp",
+                "timestamp": timestamp
+            }),
+            websocket
+        )
+
+    @staticmethod
+    async def convert_base64_to_image(image_data: str):
+        binary_data = base64.b64decode(image_data)
+        image_array = bytearray(binary_data)
+
+        # Utilisez OpenCV pour traiter l'image
+        image = cv2.imdecode(np.array(image_array), cv2.IMREAD_UNCHANGED)
+        return image
