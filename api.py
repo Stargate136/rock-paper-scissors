fa67e9353f2e80b317579f24cffc4f50deeaeb06
Christian Aucane <christianaucane1@gmail.com>
2023-12-19 12:12:42 +0100

    Create a WebSocketError and raise it for any errors in websocket_handler

--- api.py
+++ api.py
@@ -2,38 +2,23 @@ from fastapi import FastAPI, WebSocket
 from fastapi.responses import FileResponse
 from starlette.requests import Request
 
-from managers import TimeManager, GameManager, WebSocketHandler
+from managers import WebSocketHandler
 
 
 app = FastAPI()
-time_manager = TimeManager()
-game_manager = GameManager()
 
-# =============================================================================
-# GET
-# =============================================================================
 
+# ==== HTTP ====
 @app.get("/")
 def get_html(request: Request):
-    print(request.url.path)
     return FileResponse("index.html", media_type="text/html")
 
 
-@app.get("/get_server_timestamp")
-def get_server_timestamp():
-    return {"timestamp": time_manager.now()}
+# ==== WEBSOCKET ====
 
+class WebSocketError(Exception):
+    pass
 
-@app.get("/get_timestamp_after_delay")
-def get_timestamp_after_delay():
-    delay_seconds = 5
-    timestamp = time_manager.timestamp_after_delay(delay_seconds)
-    return {"timestamp": timestamp}
-
-
-# =============================================================================
-# WEB SOCKET
-# =============================================================================
 
 @app.websocket("/ws")
 async def websocket_handler(websocket: WebSocket):
@@ -45,6 +30,6 @@ async def websocket_handler(websocket: WebSocket):
             data = await websocket.receive_json()
             await websocket_instance.on_receive(websocket, data)
     except Exception as e:
-        print(f"WebSocket Error: {e}")
+        raise WebSocketError(e)
     finally:
         await websocket_instance.on_disconnect(websocket)
